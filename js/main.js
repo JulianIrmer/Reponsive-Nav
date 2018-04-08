6683a3a1a962d67b93d3a97c60b591b2950adde8
Julian Irmer <julian.irmer@gmail.comgit config --global user.email  julian.irmer@gmail.com>
2018-04-08 23:26:59 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function toggle(){
+    document.querySelector(".menu-small").classList.toggle("visible");
+    document.querySelector("#one").classList.toggle("one45deg");
+    document.querySelector("#three").classList.toggle("three-45deg");
+    document.querySelector("#two").classList.toggle("hidden");
+}
